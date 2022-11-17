7ec8aed99f5f0cd350cf1672d05cd3c2b987fb89
DianaTumasyan <117822352+DianaTumasyan@users.noreply.github.com>
2022-11-17 20:38:22 +0400

    Add files via upload
    file for testing all the functions (calling them)

---
+++ testing-file.js
@@ -0,0 +1,6 @@
+const BringFactorial = require('./MathematicalFunctions.js');
+const GCD = require('./MathematicalFunctions.js');
+const LCM = require('./MathematicalFunctions.js');
+const hexadecimal = require('./MathematicalFunctions.js');
+    
+   
